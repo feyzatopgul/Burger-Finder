1510f600a945cb0c446a107199d4a87745bde91c
Feyza Topgul <feyzatopgul@gmail.com>
2022-12-24 10:51:20 -0800

    Initial Commit

---
+++ Burger Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Burger Finder
+//
+//  Created by fyz on 12/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
